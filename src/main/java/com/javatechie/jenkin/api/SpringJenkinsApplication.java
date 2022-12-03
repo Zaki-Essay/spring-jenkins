204b797c51300f04c9ba7d97c3ae1a7ea3007fb2
zakoEssy <zakaressaiydy@gmail.com>
2022-12-03 02:56:37 +0100

    second commit

--- src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
+++ src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
@@ -1,22 +1,35 @@
 package com.javatechie.jenkin.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 @RestController
 public class SpringJenkinsApplication {
 
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
 
+	/*
 	@GetMapping("/ping")
 	public String message() {
 		return "Wao!! Application Deployed successfully in SAP Cloud..";
+	}*/
+
+	@PostConstruct
+	public void init(){
+		logger.info("init executing...");
 	}
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringJenkinsApplication.class, args);
+		logger.info("SpringJenkinsApplication executing...");
 	}
 
 }
